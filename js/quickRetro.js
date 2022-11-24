9437ef3623e099ddeaae7f5d4855765ef9993a3a
ntim <timotic.nikola.27@gmail.com>
2022-11-24 19:34:07 +0100

    add quickRetro (not finished)

---
+++ js/quickRetro.js
@@ -0,0 +1,54 @@
+// Symbol 
+// Number 
+// Null 
+// Undefined 
+// Boolean
+
+// Object 
+    // Array
+    // Function 
+
+// deklaracija
+// inicijalizacija
+// let promenljiva = vrednost
+
+let broj = 10
+let str = 'Moje ime'
+let niz = [1, 2, 3]
+
+// for(let i = 1; i < 11; i++) {
+//     console.log(i)
+// }
+
+// for(let i = 0; i < str.length; i++) {
+//     console.log(str.charAt(i))
+// }
+
+// for(let i = 0; i < niz.length; i++) {
+//     console.log(niz[i]);
+// }
+
+// niz.shift()
+// niz.unshift()
+console.log(niz.pop())
+console.log(niz);
+
+str.indexOf('o')
+str.charAt(1)
+
+function imeFunkcije(parametri) {
+    return nekaVrednost
+}
+
+function zbir(broj_1, broj_2) {
+    let zbir = broj_1 + broj_2
+    return zbir
+}
+
+let broj_1 = 1
+let broj_2 = 2 
+
+console.log(zbir(broj_1, broj_2))
+
+
+// console.log('Pozdrav');
