549d516ce924c6d95e30ad0b09d841fd211b1adb
ntim <timotic.nikola.27@gmail.com>
2022-12-01 19:46:04 +0100

    add image display options and js retro

--- js/quickRetro.js
+++ js/quickRetro.js
@@ -12,9 +12,9 @@
 // inicijalizacija
 // let promenljiva = vrednost
 
-let broj = 10
-let str = 'Moje ime'
-let niz = [1, 2, 3]
+// let broj = 10
+// let str = 'Moje ime'
+// let niz = [1, 2, 3]
 
 // for(let i = 1; i < 11; i++) {
 //     console.log(i)
@@ -30,25 +30,88 @@ let niz = [1, 2, 3]
 
 // niz.shift()
 // niz.unshift()
-console.log(niz.pop())
-console.log(niz);
+// console.log(niz.pop())
+// console.log(niz);
 
-str.indexOf('o')
-str.charAt(1)
+// str.indexOf('o')
+// str.charAt(1)
 
-function imeFunkcije(parametri) {
-    return nekaVrednost
-}
+// function imeFunkcije(parametri) {
+//     return nekaVrednost
+// }
 
-function zbir(broj_1, broj_2) {
-    let zbir = broj_1 + broj_2
-    return zbir
-}
+// function zbir(broj_1, broj_2) {
+//     let zbir = broj_1 + broj_2
+//     return zbir
+// }
 
-let broj_1 = 1
-let broj_2 = 2 
+// let broj_1 = 1
+// let broj_2 = 2 
 
-console.log(zbir(broj_1, broj_2))
+// console.log(zbir(broj_1, broj_2))
 
 
 // console.log('Pozdrav');
+
+// if(uslov) {
+
+// } else {
+
+// }
+
+// if() {
+
+// } else if() {
+
+// }
+
+// if() {
+
+// }
+
+// if() {
+
+// }
+
+// let mesec = 'januar'
+
+// switch(mesec) {
+//     case 'januar':
+//     case 'mart':
+//         console.log('Ima 31 dan');
+//         break;
+//     case 'februar':
+//         console.log('Ima 28 dana')
+//         break     
+//     default:
+//         console.log('Proverite da li ste dobro uneli mesec')
+// }
+
+// let obj = {
+//     name: 'John',
+//     surname: 'Doe',
+//     age: 30,
+//     hobi: ['kosarka', 'fotografija']
+// }
+
+// console.log(typeof obj.name)
+// console.log(obj.hobi[obj.hobi.length - 1])
+
+// console.log(`ovo je moj drug ${obj.name} ${obj.surname}`)
+
+function imeFunkcije(parametri) {
+    //logika f-je
+    return vrednost
+}
+
+function zbir(num1, num2) {
+    return num1 + num2
+}
+
+let num1 = 10
+let num2 = 20
+
+let num3 = 30
+let num4 = 40
+
+console.log(zbir(num3, num4))
