915a4075c807df56a79fa9b462745c16ac0e5e1a
lukovso <lukovic.sofija@gmail.com>
2023-03-20 23:58:21 +0100

    new files

---
+++ domaci/vezba_zadaci/vezba_zadaci.js
@@ -0,0 +1,52 @@
+const div = document.querySelector('div')
+// 5.	Ispisati u konzoli današnji datum u formatima dd.mm.yyyy. i yyyy/mm/dd.
+
+// let dan = new Date().getDate()
+// let mesec = (new Date().getMonth + 1)
+// let godina = new Date().getFullYear()
+
+// let vreme = `${dan}-${mesec}-${godina}`
+// console.log(vreme)
+
+
+// 6.	Data je promenljiva u kojoj je ukupan broj evra koji posedujemo, kao i promenljiva u kojoj je vrednost trenutnog kursa.Odrediti koliko ćemo imati dinara nakon razmene.Uraditi isto i za konverziju iz dinara u evre.
+
+// function konverzija(){
+//     let kon = 0
+//     kon = evri * kurs
+//     return kon
+// }
+// let evri = 50
+// let kurs = 118
+// console.log(konverzija())
+
+// 8. Za unetu zapreminu proizvoda, u paragrafu ispisati “Throw away”, ukoliko je proizvod zapremine veće od 100ml. U suprotnom ispisati “Pack up”.
+
+// let zapremina = 150
+
+// if(zapremina > 100){
+//     console.log('throw away');
+// }else{
+// console.log('pick up')
+// }
+
+// 9. Za unetu temperaturu u paragrafu, ispisati “Temperatura u plusu” crvenom bojom ili “Temperatura u minusu” plavom bojom. Ukoliko je temperatura nula, računati kao temperaturu u plusu.
+
+// let temperatura = 50
+// const pParagraf = document.createElement('p')
+
+// if(temperatura >= 0){
+//     pParagraf.textContent = 'temperatura u plusu'
+//     pParagraf.style.color = 'red'
+// }else{
+//     pParagraf.textContent = 'temperatura u minusu'
+//     pParagraf.style.color = 'blue'
+//     div((pParagraf))
+// }
+
+// 10. U odnosu na pol koji je korisnik uneo u promenljivu  (“m” za muški pol ili “z” za ženski pol)  na ekranu prikazati odgovarajući avatar (odgovarajuću sličicu).
+
+// let pol = m
+// if(pol === m){
+    
+// }
