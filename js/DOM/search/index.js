1a6572a8edca471440846a936c8c0508072841bd
lukovso <lukovic.sofija@gmail.com>
2023-06-05 21:11:07 +0200

    search homework

--- js/DOM/search/index.js
+++ js/DOM/search/index.js
@@ -16,8 +16,77 @@ const postovi = [
     }
 ]
 
+
 postovi.sort((a, b) => b.numLikes - a.numLikes)
 
-console.log(postovi)
+const divIspis = document.querySelector('.post-container')
 
-displayPost()
+// const ispisPostova = (postovi) =>{
+    //     postovi.forEech(post =>{
+        
+        //         const noviDiv = document.createElement('div')
+        
+        //         const pPostName = document.createElement('p')
+        //         pPostName.textContent = post.postName
+        
+        //         const pNumLikes = document.createElement('p')
+//         pNumLikes.textContent = post.numLikes
+
+//         const img = document.createElement('img')
+//         img.value = post.img
+        
+//         noviDiv.append(pPostName,pNumLikes,img)
+//         divIspis.appendChild(noviDiv)
+
+//     })
+// }
+
+const ispisPostova = (postovi) => {
+    postovi.forEach(post => {
+
+        const div = document.createElement('div')
+        div.className = 'post'
+        
+        const img = document.createElement('img')
+        img.src = post.img
+        
+        const divLike = document.createElement('div')
+        divLike.className = 'likes'
+        
+        const pNumLikes = document.createElement('p')
+        pNumLikes.textContent = post.numLikes
+        pNumLikes.className = 'post-name'
+        
+        const button = document.createElement('button')
+        button.textContent = 'Like'
+        button.className = 'btnLike'
+        
+        divLike.append(pNumLikes,button)
+        
+        const pPostName = document.createElement('p')
+        pPostName.textContent = post.postName
+
+        function like (pNumLikes){
+            pNumLikes = +pNumLikes.textContent + 1
+            return pNumLikes
+        }
+        
+        button.addEventListener('click', () => {
+            pNumLikes.textContent = like(pNumLikes)
+            // post.numLikes.sort((a,b)=> b.numLikes - a.numLikes)
+            // ispisPostova(postovi)
+        })
+
+        div.append(img,divLike,pPostName)
+        divIspis.appendChild(div)
+    })
+}
+ispisPostova(postovi)
+
+
+// ispisPostova(postovi)
+
+
+// console.log(postovi)
+
+// displayPost()
