7f3f8adffc6be380970b5b287ae6e65fd4fa0f79
lukovso <lukovic.sofija@gmail.com>
2023-06-19 22:34:03 +0200

    homework forms

---
+++ js/DOM/vezbaforma/forma.js
@@ -0,0 +1,120 @@
+const forma = document.querySelector('form')
+const inputUsename = document.querySelector('#username')
+const inputPassword = document.querySelector('#password')
+const poruka = document.querySelector('.poruka')
+const naslovPoruka = document.querySelector('.naslovPoruka')
+const pVreme = document.querySelector('.vreme')
+const svg = document.querySelector('svg')
+const randomPoruka = document.querySelector('.randomPoruka')
+
+let korisnici = [
+    {
+        user: '123',
+        pass: '123'
+    },
+    {
+        user: 'admin',
+        pass: 'admin'
+    },
+    {
+        user: 'korisnik',
+        pass: 'korisnik'
+    }
+]
+
+
+
+
+function vreme(pVreme){
+    const sati = new Date().getHours()
+    const minuti = new Date().getMinutes()
+    const sekunde = new Date().getSeconds()
+
+    if(sati < 10)
+    sati = '0' + sati
+    if(minuti < 10)
+    minuti = '0' + minuti
+    if(sekunde < 10)
+    sekunde = '0' + sekunde
+    
+    pVreme.innerHTML = `${sati}:${minuti}:${sekunde}`
+    return setTimeout(()=>{
+        vreme(pVreme)
+    },1000)
+}
+
+
+
+forma.addEventListener('submit', (el) =>{
+    el.preventDefault()
+
+    if(inputUsename.value.trim() === ''){
+        poruka.textContent = 'Morate uneti vas username'
+        inputUsename.style.border = '1px solid red'
+        setTimeout(()=>{
+            poruka.textContent = ''
+            inputUsename.style.border = '1px solid white'
+            inputUsename.value = ''
+        },3000)
+        return
+    }
+    if(inputPassword.value.trim() === ''){
+        poruka.textContent = 'Morate uneti vas password'
+        inputPassword.style.border = '1px solid red'
+        return
+    }
+
+
+    
+    let ulogovan = false
+    for(let i = 0; i<korisnici.length; i++){
+        if(inputUsename.value === korisnici[i].user){
+            if(inputPassword.value === korisnici[i].pass){
+                inputUsename.style.border = '1px solid green'
+                inputPassword.style.border = '1px solid green'
+                ulogovan = true
+                setTimeout(()=>{
+                        inputUsename.style.border = '1px solid white'
+                        inputUsename.value = ''
+                        inputPassword.style.border = '1px solid white'
+                        inputPassword.value = ''
+                },3000)
+                naslovPoruka.innerHTML = `DOBRO VECE, dobrodosao! ${inputUsename.value}`
+                break
+            }break
+            }
+    }
+    if(!ulogovan){
+        poruka.textContent = 'Proverite unos za vas username i password'
+        inputUsename.style.border = '1px solid red'
+        inputPassword.style.border = '1px solid red'
+        setTimeout(()=>{
+            poruka.textContent = ''
+                inputUsename.style.border = '1px solid white'
+                inputUsename.value = ''
+                inputPassword.style.border = '1px solid white'
+                        inputPassword.value = ''
+        },3000)
+        return
+    }
+})
+
+
+let poruke = [
+    'abc',
+    '123',
+    'aaaaaaa'
+]
+
+
+let random = poruke[Math.floor(Math.random()*poruke.length)]
+
+
+// console.log(random);
+
+svg.addEventListener("click",()=>{
+     randomPoruka.innerHTML = random
+})
+
+
+vreme(pVreme)
